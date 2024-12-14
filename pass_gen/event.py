334ea1dd3ddc8e1822a50c3ec18f7206a7d707f1
Kazuki Adachi <kazuki.adachi.xy@gmail.com>
2024-12-14 12:21:51 +0900

    fix error

--- pass_gen/event.py
+++ pass_gen/event.py
@@ -4,7 +4,7 @@ from pass_gen import pass_gen
 from js_utils import copy_to_clipboard, display_message
 
 
-def generate_pass():
+def generate_pass(event):
     limited_characters = document.getElementById("limited_characters").checked
     p = pass_gen(document.getElementById("input").value,
                  limited_characters=limited_characters)
